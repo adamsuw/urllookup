2cee050f282def1c3f82f9a23ea5f8d1492032ad
adamsuw <oyairouwa@yahoo.com>
2021-02-22 18:42:26 -0500

    reconnect postgres container

--- main/routes/index.js
+++ main/routes/index.js
@@ -1,15 +1,27 @@
 var express = require('express');
+const UserSystem = require('../bin/services/UserSystem');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
-router.get('/info', (req, res) => {
-  
-  res.send({info: "Hello Cisco"})
-})
+router.get('/info', async (req, res, next) => {
+  const {
+    getInfo
+  } = UserSystem();
+  const result = await getInfo();
+  res.send(result);
+});
 
+router.get('/info/save', (req, res, next) => {
+  const {
+    saveInfo
+  } = UserSystem();
+  res.send(saveInfo());
+});
 
 module.exports = router;
